fa30f88a42371c8395862556fcb0381e3981334b
Tom Hicks <hickst@tohono.com>
2022-07-13 13:00:29 -0700

    Add some more examples of conversion to base 10.

--- src/baseConv/core.clj
+++ src/baseConv/core.clj
@@ -32,7 +32,15 @@
 
 (defn -main [& args]
   (let [ from62 (convertFromBase 62)
-         from16 (convertFromBase 16) ]
-    (println "DTVD3O in base 62 is:" (from62 "DTVD3O") "in base 10")
-    (println "FF in base 16 is:" (from16 "FF") "in base 10")
+         from16 (convertFromBase 16)
+         from2 (convertFromBase 2) ]
+    (println "'10' in base 62 is" (from62 "10") "in base 10")
+    (println "'zz' in base 62 is" (from62 "zz") "in base 10")
+    (println "'DTVD3O' in base 62 is" (from62 "DTVD3O") "in base 10")
+    (println)
+    (println "'10' in base 16 is" (from16 "10") "in base 10")
+    (println "'FF' in base 16 is" (from16 "FF") "in base 10")
+    (println)
+    (println "'10' in base 2 is" (from2 "10") "in base 10")
+    (println "'1111' in base 2 is" (from2 "1111") "in base 10")
 ))
